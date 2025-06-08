cf98f15f3a925f51e4bc19ec7b15b88c836ac883
fro29 <96358466+fro29@users.noreply.github.com>
2025-06-08 01:10:16 -0600

    deployment class

---
+++ app/data.py
@@ -0,0 +1,80 @@
+data = {
+    'name': 'Francisco Rodríguez Osorio',
+    'education': [
+        {'Skill': 'Programming languages:',
+         'Description': ' ',
+         'Skill Detail': ['Python','Java ','Cobol', 'C/C++','Javascript','TypeScript']
+        },
+        {'Skill': 'Version control systems and repositories:',
+         'Description': ' ',
+         'Skill Detail': ['Git','Github','Gitlab','Bitbucket']
+        },
+        {'Skill': 'Cloud computing platforms:',
+         'Description': ' ',
+         'Skill Detail': ['AWS', 'Microsoft Azure'] 
+        },
+        {'Skill': 'Database Managers:',
+         'Description': '',
+         'Skill Detail': ['MySQL', 'MongoDB'] 
+        },
+        {'Skill': 'Operating Systems:',
+         'Description': '',
+         'Skill Detail': ['Windows', 'Linux (Linux Shell Basics)',]
+        },
+        {'Skill': 'Internet protocols:',
+         'Description': '',
+         'Skill Detail': ['TCP/IP', 'ARP ','ICMP ','DNS','HTTP/HTTPS'] 
+        },
+        {'Skill': 'CI/CD:',
+         'Description': '',
+         'Skill Detail': ['CI server (Jenkins)', 'Terraform', 'Docker',  'Kubernets'] 
+        },
+        {'Skill': 'Frameworks:',
+         'Description': '',
+         'Skill Detail': ['Flask', 'Django', 'React JS', 'Angular'] 
+        },
+        {'Skill': 'Machine Learning (IA/ML):',
+         'Description': 'Deep learning with neuronal networks',
+         'Skill Detail': ['TensorFlow', 'Scikit‑learn', 'Keras'] 
+        },
+        {'Skill': 'Other knowladge:',
+         'Description': '',
+         'Skill Detail': ['HTML','CSS3'] 
+        }
+        
+    ],
+    'projects':[
+        {'Project': 'Speech emotion recognition through a web application',
+         'Description': 'Degreework (Thesis). UPIITA, IPN',
+         'Skill Detail': ['Analyzed audio data from human voices to identify patterns and features that distinguish different emotional states.', 
+                          'Web application developed in Flask, HTML, CSS3 and JavaScript with MVC (Model‑View‑Controller) Architecture.',
+                          'Designed, tested, deployed, and maintained an AI model using Machine Learning techniques through Deep Learning with Convolutional Neural Networks (CNNs).'
+                          ],
+         'Technical': 'Python (Flask | TensorFlow | Scikit‑learn | Keras | Numpy | Pandas | Scipy | Matplotlib), Machine Learning with Convolutional Neural Networks, MySQL, Data analysis through statistical programming) and MySQL.'
+        },
+        {'Project': 'Online store (E‑COMMERCE)',
+         'Description': 'Personal Projects',
+         'Skill Detail': ['Web application developed in JavaScript, Express, Ajax among other technologies with Client‑Server architecture', 
+                          'User and product administration through non‑relational database (MongoDB).'],
+          'Technical': 'HTML, JavaScript and MongoDB (Atlas). '
+        }
+    ],
+    'Educ':[
+        {
+            'Carrer': 'Telematics Engineering',
+            'Place': 'CDMX, México',
+            'Data': 'Aug 2019 ‑ July 2024',
+            'Unitity':'Instituto Politécnico Nacional (IPN)',
+            'School':'Unidad Profesional Interdiciplinaria en Ingeniería y Tecnologías Avanzadas (UPIITA)'       
+        },
+        {
+            'Carrer': 'Digital Systems Technician',
+            'Place': 'CDMX, México',
+            'Data': 'Aug 2016 ‑ July 2019',
+            'Unitity':'Instituto Politécnico Nacional (IPN)',
+            'School':'C.E.C.y.T. No.1 "Gonzalo Vazquez Vela"'
+            
+        }
+        
+    ]
+}
